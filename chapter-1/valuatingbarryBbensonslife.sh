013f5b94b3098ec956af9420f2f5931f66d2fb71
JammieCoder <139925958+JammieCoder@user.noreply.github.com>
2024-04-30 18:37:59 +0100

    shell command to run 1-8 & 1-9 on bee movie script

---
+++ chapter-1/valuatingbarryBbensonslife.sh
@@ -0,0 +1,4 @@
+gcc 1-8-counting-chars.c -o build/1-8-counting-chars
+gcc 1-9-blank-replacer.c -o build/1-9-blank-replacer
+./build/1-9-blank-replacer < beeees.txt
+./build/1-8-counting-chars < beeees.txt
